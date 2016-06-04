62ab3fd7b4a73689a7345a04d8b99f7060de2e83
Alec Burgdorf <aburgd@users.noreply.github.com>
2016-06-04 21:07:48 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express")
+var path = require("path")
+var moment = require("moment")
+
+var app = express
+var port = process.env.PORT || 3500
+
+app.set('port', port)
+app.use(express.static(path.resolve(__dirname, 'client')))
+app.get('/:timestamp', function(req, res) {
+    let time = moment(req.params.timestamp, 'MMMM DD, YYYY', true)
+    if (!time.isValid()) time = moment.unix(req.params.timestamp)
+    if (!time.isValid()) {
+      res.json({
+        'humanreadable': null,
+        'unix' : null
+      })
+    }
+    
+    res.json({
+      'humanreadable': time.format('MMMM DD, YYYY')
+    , 'unix': time.format('X')
+    })
+})
+
+app.listen(port, () => {
+  console.log("Server is listening on ${port}")
+})
